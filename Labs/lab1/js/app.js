b3d85b94e870f04131a0b5e66e14b8ecf902654b
CameronToddIU <97647716+CameronToddIU@users.noreply.github.com>
2022-01-13 17:12:41 -0500

    First Composition

---
+++ Labs/lab1/js/app.js
@@ -0,0 +1,17 @@
+function preload() {
+    // Ensure the .ttf or .otf font stored in the assets directory
+    // is loaded before setup() and draw() are called
+    font = loadFont('assets/monster/Montserrat-Black.ttf');
+  }
+
+function setup() {
+    createCanvas(800,600);
+    textFont(font);
+    textSize(30);
+    textAlign(CENTER, CENTER);
+}
+
+function draw() {
+
+    circle(mouseX,mouseY,frameCount/10);
+}
