850b5935d27307869e1d5e2eb23429f353c12ba0
CameronToddIU <97647716+CameronToddIU@users.noreply.github.com>
2022-01-13 17:20:18 -0500

    Update app.js

--- Labs/lab1/js/app.js
+++ Labs/lab1/js/app.js
@@ -1,14 +1,14 @@
-function preload() {
-    // Ensure the .ttf or .otf font stored in the assets directory
-    // is loaded before setup() and draw() are called
-    font = loadFont('assets/monster/Montserrat-Black.ttf');
-  }
+// function preload() {
+//     // Ensure the .ttf or .otf font stored in the assets directory
+//     // is loaded before setup() and draw() are called
+//     font = loadFont('assets/monster/Montserrat-Black.ttf');
+//   }
 
 function setup() {
     createCanvas(800,600);
-    textFont(font);
-    textSize(30);
-    textAlign(CENTER, CENTER);
+    // textFont(font);
+    // textSize(30);
+    // textAlign(CENTER, CENTER);
 }
 
 function draw() {
