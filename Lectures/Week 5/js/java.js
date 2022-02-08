e88b2f13cc6c5a1868cec249ec3146e9eed48a74
CameronToddIU <97647716+CameronToddIU@users.noreply.github.com>
2022-02-08 18:40:14 -0500

    put index in folder

---
+++ Lectures/Week 5/js/java.js
@@ -0,0 +1,15 @@
+let Test = document.getElementById("Test");
+
+Test.style.width = "100px";
+Test.style.height = "100px";
+Test.style.backgroundColor = "green";
+
+let width = 100;
+let height = 100;
+
+function increaseSize(){
+    
+
+
+}
+
