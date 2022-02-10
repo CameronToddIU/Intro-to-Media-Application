64ddc2680d4182ce7b4439a9701e927f2f394a24
CameronToddIU <97647716+CameronToddIU@users.noreply.github.com>
2022-02-10 17:59:40 -0500

    comments

--- Lectures/Week 5 V2/js/app2.js
+++ Lectures/Week 5 V2/js/app2.js
@@ -16,10 +16,12 @@ function setup()
 function draw()
 {
     background(255);
+    //creats a circle that offsets from the mouse's x position
     circle(mouseX + obj.offSetX,mouseY,obj.size);
 
     if(mouseIsPressed) 
 {
+    //while mouse is pressed bring circle x position to 0
     if(obj.offSetX >=0)
     {
     obj.offSetX -= 1;
@@ -27,6 +29,7 @@ function draw()
 }
 
 else {
+    //while mouse is not pressed increase circle x position
     obj.offSetX += 1;
 }
 
--- Lectures/Week 5 V2/js/app3.js
+++ Lectures/Week 5 V2/js/app3.js
@@ -1,3 +1,5 @@
+
+//object properties for circle
 let obj = {
     x:0,
     y:0,
@@ -18,23 +20,26 @@ function draw()
 {
     background(255);
     fill('black');
+    //calls circle check
     circleCheck();
 
 }
 
 function circleCheck(){
 
-    
+    //creates circle
     circle(obj.x+=obj.speedx,obj.y + obj.speedy,obj.size);
 
     //changes the x speed to a negative number once it reaches a border
     if (obj.x >=800)
     {
+        //changes circle x speed to negative if it reaches canvas edge
         obj.speedx*=-1;
         console.log("change x speed");
     }
     if (obj.x <=0)
     {
+        //changes circle x speed to positive if it reaches canvas edge
         obj.speedx*=-1;
         console.log("change x speed");
     }
@@ -42,11 +47,13 @@ function circleCheck(){
     //changes the y speed to a negative number once it reaches a border
     if (obj.y >=600)
     {
+        //changes circle y speed to negative if it reaches canvas edge
         obj.speedy*=-1;
         console.log("change y speed");
     }
     if (obj.y <=0)
     {
+        //changes circle y speed to positive if it reaches canvas edge
         obj.speedy*=-1;
         console.log("change y speed");
     }
